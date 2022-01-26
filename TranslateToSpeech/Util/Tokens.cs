082193bcd78dd1e96b8d78ef2ec2af26448366b2
Linda Abdullah <linda.abdullah.014@gmail.com>
2022-01-26 21:37:56 +0300

    hard coded tokens to env vars

--- TranslateToSpeech/Util/Tokens.cs
+++ TranslateToSpeech/Util/Tokens.cs
@@ -7,12 +7,12 @@ namespace TranslateToSpeech
 {
 	public class Tokens
 	{
-		public static readonly string TranslateKey = "074650efa5a24dd0a5611397acb2483d"; // Environment.GetEnvironmentVariable("TRANSLATE_KEY")
-		public static readonly string TranslateRegion = "eastus";//Environment.GetEnvironmentVariable("TRANSLATE_REGION")
+		public static readonly string TranslateKey = Environment.GetEnvironmentVariable("TRANSLATE_KEY");
+		public static readonly string TranslateRegion = Environment.GetEnvironmentVariable("TRANSLATE_REGION");
 		public static readonly string TranslateEndpoint = "https://api.cognitive.microsofttranslator.com";
 
-		public static readonly string SpeechKey = "67327444b03c4eeb8f828fba9b42cf48";//Environment.GetEnvironmentVariable("SPEECH_KEY").ToString()
-		public static readonly string SpeechRegion = "eastus";//Environment.GetEnvironmentVariable("SPEECH_REGION")
+		public static readonly string SpeechKey = Environment.GetEnvironmentVariable("SPEECH_KEY");
+		public static readonly string SpeechRegion = Environment.GetEnvironmentVariable("SPEECH_REGION");
 		public static readonly string SpeechEndpoint = $"https://{SpeechRegion}.tts.speech.microsoft.com";
 	}
 }
